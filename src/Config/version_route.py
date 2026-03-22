c1cadc8375e80af3ce8e417777b797d35a33c946
JRomero-G <jnoel.romerog@gmail.com>
2026-03-21 21:56:10 -0600

    Prueba de actualizacion con github releases

--- src/Config/version_route.py
+++ src/Config/version_route.py
@@ -8,5 +8,9 @@ router = APIRouter(prefix="/config", tags=["Configuración"])
 def get_version():
     return {
         "version": CURRENT_VERSION,
-        "url": "https://github.com/TU_USUARIO/TU_REPO/releases/download/v1.0.7/Installer_Gestorex_v1.0.7.exe"
+        # Esta url se actualiza dependiendo de la release publicada
+        # v1.0.7 → Release v1.0.7 → URL .../v1.0.7/Installer_Gestorex_v1.0.7.exe
+        # v1.0.8 → Release v1.0.8 → URL .../v1.0.8/Installer_Gestorex_v1.0.8.exe -> para futura version
+        "url": "https://github.com/mmedinafv/UIPATH/releases/download/v1.0.7/Installer_Gestorex_v1.0.7.exe"
+        
     }
