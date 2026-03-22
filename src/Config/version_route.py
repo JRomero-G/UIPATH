65b43b881ab6ae85aba73b7beb017a47caef0e86
JRomero-G <jnoel.romerog@gmail.com>
2026-03-22 00:26:13 -0600

    Ajustes  los archivos de actualizacion

--- src/Config/version_route.py
+++ src/Config/version_route.py
@@ -4,12 +4,13 @@ from src.Config.version import CURRENT_VERSION
 
 router = APIRouter(prefix="/config", tags=["Configuración"])
 
+GITHUB_REPO = "mmedinafv/UIPATH"
+
 @router.get("/version")
 def get_version():
     return {
         "version": CURRENT_VERSION,
-        # Esta url se actualiza dependiendo de la release publicada
-        # v1.0.7 → Release v1.0.7 → URL .../v1.0.7/Installer_Gestorex_v1.0.7.exe
-        # v1.0.8 → Release v1.0.8 → URL .../v1.0.8/Installer_Gestorex_v1.0.8.exe -> para futura version
-        "url": "https://github.com/mmedinafv/UIPATH/releases/download/v1.0.8/Installer_Gestorex_v1.0.8.exe"   
+        "tag": f"v{CURRENT_VERSION}",
+        "filename": f"Installer_Gestorex_v{CURRENT_VERSION}.exe",
+        "repo": GITHUB_REPO
     }
