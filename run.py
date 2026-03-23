a687bfe3f279ca966bf19bd36bd7b6088fa5982f
JRomero-G <jnoel.romerog@gmail.com>
2026-03-23 01:52:33 -0600

    Quite unos prints innesearios

--- run.py
+++ run.py
@@ -11,21 +11,15 @@ else:
 from dotenv import load_dotenv
 load_dotenv(env_path)
 
-print(f"[RUN] BACKEND_URL cargado: {os.getenv('BACKEND_URL')}")  # verificar
-
 log_path = os.path.join(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else ".", "error_log.txt")
 
 try:
-    print("[RUN] Iniciando...")
     from UI.main import Iniciar
-    print("[RUN] Import exitoso")
 
     if __name__ == "__main__":
         Iniciar()
 
 except Exception as e:
     error_msg = traceback.format_exc()
-    print(f"[RUN] ERROR: {error_msg}")
     with open(log_path, "w") as f:
         f.write(error_msg)
-    input("Presiona Enter para cerrar...")
