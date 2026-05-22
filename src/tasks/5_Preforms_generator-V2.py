cdb8a7336b9aa4a5470179d876faa1a15f6e4c51
JRomero-G <jnoel.romerog@gmail.com>
2026-05-22 16:08:08 -0600

    Correccion

--- src/tasks/5_Preforms_generator-V2.py
+++ src/tasks/5_Preforms_generator-V2.py
@@ -23,7 +23,7 @@ MYSQL_CONFIG = {
 }
 
 
-GEMINI_CREDENTIALS_PATH = Global.RENDER_CRENDENTIALS_JSON
+GEMINI_CREDENTIALS_PATH = Global.GEMINI_CRENDENTIALS
 BUCKET_NAME             = Global.BUCKET_NAME
 BUCKET_FOLDER           = "Documentos de Contratación"
 AI_MODEL                = "gemini-2.5-pro"
